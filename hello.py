f3dc4339a023316183b6560b07498850e2fa999a
Corey <corey.r.girard@gmail.com>
2018-02-06 17:45:58 -0500

    Cleaning, and version increment script

--- hello.py
+++
@@ -1,3 +0,0 @@
-print('hello')
-#import example
-#print('your number is...{0}'.format(example.fetch_forty_two()))
--- validate.sh
+++ validate.sh
@@ -3,7 +3,7 @@ REPO="https://github.com/coreygirard/breeze-ci-example/archive/master.zip"
 rm -rf build
 
 mkdir build
-mkdir build/shared
+#mkdir build/shared
 
 curl "$REPO" -L -o build/latest.zip
 unzip build/latest.zip -d build/
@@ -16,24 +16,7 @@ docker rm breeze-container
 docker rmi breeze-image
 
 docker build -t breeze-image -f "./Dockerfile" --no-cache --build-arg test_path="$TEST_PATH" ./
-#docker run --name breeze-container breeze-image
 docker run --name breeze-container --rm -i -t breeze-image
-
-#docker start breeze-container
-#docker attach breeze-container
-#docker exec breeze-container ls
-#mkdir reports
-#touch reports/temp.txt
-#ls ./reports
-#docker exec breeze-container bash
-#ls build/latest/example
-#coverage run build/latest/example/test_example.py
-
-#docker exec breeze-container touch temp.txt
-#docker exec breeze-container ls
-
-#docker build --no-cache -t --build-arg test_path="$TEST_PATH" breeze-latest .
-#docker run -v "$PWD/build/shared:/shared" breeze-latest
 
 TEST_RESULT=$?
 echo "Received: $TEST_RESULT"
@@ -43,4 +26,7 @@ else
   echo "Tests failed. Not deploying."
 fi
 
+cd build/latest
+python setup.py sdist bdist_wheel
+cd ../..
 #rm -rf build
