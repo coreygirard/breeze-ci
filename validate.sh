eed585424c84040a7b08a4448309ec6a1cc0f0d2
Corey <corey.r.girard@gmail.com>
2018-02-05 21:49:07 -0500

    Conditionals

--- validate.sh
+++ validate.sh
@@ -5,14 +5,15 @@ mv build/breeze-ci-example-master build/latest
 cp hello.py build/latest/hello.py
 
 docker build -t breeze-latest .
+docker run breeze-latest
 
+TEST_RESULT=$?
+echo "result: $?"
 
-text=`docker run breeze-latest`
-echo "text is $? $text" 
+if [ $TEST_RESULT -eq "0" ]; then
+  echo "Tests passed. Deploying..."
+else
+  echo "Tests failed. Not deploying."
+fi
 
 rm -rf build
-
-#rm build/latest/*
-#rm build/latest/.gitignore
-#rmdir build/latest
-#rm build/latest.zip
