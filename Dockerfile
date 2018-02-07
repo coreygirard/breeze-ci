856c64cedf7ca1278438a75c9f16622df2a207d0
Corey <corey.r.girard@gmail.com>
2018-02-07 00:14:29 -0500

    Tidying

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,10 @@ RUN pip install coverage
 
 ARG test_path
 CMD [ "python", $test_path ]
+
+CMD coverage run /src/example/test_example.py
+CMD coverage annotate -d /src/example/
+
+CMD echo "hey" > hello.txt,cover
+
 CMD echo "hello from Docker" > test.txt
--- val.sh
+++ val.sh
@@ -1,6 +1,7 @@
 REPO="https://github.com/coreygirard/breeze-ci-example/archive/master.zip"
 
 rm -rf build
+rm -rf dump
 
 mkdir build
 #mkdir build/shared
@@ -20,13 +21,16 @@ docker rm breeze-container
 docker rmi breeze-image
 
 docker build -t breeze-image -f "./Dockerfile" --no-cache --build-arg test_path="$TEST_PATH" ./
-docker run -d --name breeze-container --rm -i -t breeze-image
-docker cp breeze-container:test.txt ./
+docker run -d --name breeze-container -i -t breeze-image
+docker cp breeze-container:/ ./dump
+#docker cp breeze-container:coverage_report/example.py,cover ./
+
 
 TEST_RESULT=$?
 echo "Received: $TEST_RESULT"
 if [ $TEST_RESULT -eq "0" ]; then
   echo "Tests passed. Deploying..."
+
   python3 increment_pypi.py build/latest/setup.py
   cd build/latest
   python setup.py sdist bdist_wheel
@@ -35,4 +39,8 @@ else
   echo "Tests failed. Not deploying."
 fi
 
-#rm -rf build
+docker stop breeze-container
+docker rm breeze-container
+docker rmi breeze-image
+rm -rf build
+rm -rf dump
