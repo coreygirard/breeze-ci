9799383dc97da88d429eacacf350ccdc2a916dc0
Corey <corey.r.girard@gmail.com>
2018-02-14 14:35:23 -0500

    Trimming

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
 FROM python:alpine
 
 ADD /build/latest /src
-ADD /build/latest/example /
 
 RUN pip install coverage
 
 ARG test_path
 CMD [ "python", $test_path ]
 
-#CMD coverage run ./test_example.py
-#CMD sudo coverage annotate -d ./
-
-#CMD chmod 777 ./src/example
-#CMD echo "hey" > ./src/example/hello.txt
-
-#CMD echo "hello from Docker" > ./test.txt
-
-CMD coverage run ./test_example.py; coverage annotate -d /report/
+CMD coverage run ./src/example/test_example.py; coverage annotate -d /report/
--- val.sh
+++ val.sh
@@ -6,30 +6,20 @@ rm -rf report
 
 mkdir build
 mkdir build/shared
+mkdir report
 
 curl "$REPO" -L -o build/latest.zip
 unzip build/latest.zip -d build/
 mv build/breeze-ci-example-master build/latest
 
 read -d $'\x04' TEST_PATH < "./build/latest/breeze.yml"
-echo " "
-echo " "
 echo "Executing tests from: $TEST_PATH"
-echo " "
-echo " "
 
 docker rm breeze-container
 docker rmi breeze-image
 
 docker build -t breeze-image -f "./Dockerfile" --no-cache --build-arg test_path="$TEST_PATH" ./
-#docker run -d --name breeze-container -i -t breeze-image
-mkdir report
 docker run -d --name breeze-container -v $(pwd)/report:/report -i -t breeze-image
-
-#docker cp breeze-container:/ ./dump
-docker logs breeze-container
-#docker cp breeze-container:coverage_report/example.py,cover ./
-
 
 TEST_RESULT=$?
 echo "Received: $TEST_RESULT"
