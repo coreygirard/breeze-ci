d0eefb2939f4c27dc6946a03326d156b42792dba
Corey <corey.r.girard@gmail.com>
2018-02-17 11:48:21 -0500

    Testing

--- generate_html_report.py
+++ generate_html_report.py
@@ -2,7 +2,33 @@ import os
 import sys
 import re
 from pprint import pprint
+import json
 
+
+def make_rename_lookup():
+    raw_path = '/Users/coreygirard/Documents/GitHub/breeze-ci/report/'
+    d = {}
+
+    with open(os.path.join(raw_path, 'ls_report.txt')) as f:
+        lines = [line.strip('\n') for line in f]
+
+    indices = [i for i,e in enumerate(lines) if e == '']
+    indices = [-1] + indices + [len(lines)]
+
+    lines = [lines[a+1:b] for a, b in zip(indices,indices[1:])]
+    for e in lines:
+        a, b = e[0], e[1:]
+
+        while a[-1] in '/:':
+            a = a[:-1]
+        a = a + '/'
+
+
+        for i in b:
+            filepath = a+i
+            d[filepath.replace('/','_')] = filepath
+
+    return d
 
 def is_covered(r):
     return [(e[0],e[2:]) for e in r]
@@ -28,7 +54,7 @@ lookup = {'>':'    <span style="font-family: Courier New;">{0}</span>'
               '<span style="font-family: Courier New; background-color:#ef9a9a">{1}</span><br>',
           ' ':'    <span style="font-family: Courier New;">{0}</span>'
               '<span style="font-family: Courier New;">{1}</span><br>'}
-def to_html(rep):
+def to_html2(rep):
     with open('/Users/coreygirard/Documents/GitHub/breeze-ci/data/report_template.html', 'r') as f:
         template = f.read()
 
@@ -41,6 +67,17 @@ def to_html(rep):
 
     return template.replace('{{text}}', lines)
 
+def to_html(rep):
+    pprint(rep)
+    lines = []
+    for i,(a,b) in enumerate(rep, start=1):
+        num = i
+        text = b.replace(r' ',r'&nbsp')
+        lines.append(lookup[a].format(num,text))
+    lines = '\n'.join(lines)
+
+    #return template.replace('{{text}}', lines)
+
 def to_stats(rep):
     d = {}
     for e,_ in rep:
@@ -52,6 +89,27 @@ def to_stats(rep):
 
 link = '<a href="/file/{0}">{1}</a><br>'
 row = '<tr><th>{0}</th><th>{1}</th><th>{2}</th><th>{3}%</th></tr>'
+def build_main2(stats, lookup):
+    with open('/Users/coreygirard/Documents/GitHub/breeze-ci/data/index_template.html', 'r') as f:
+        template = f.read()
+
+    temp = []
+    for k,v in stats.items():
+        red = v.get('!', 0)
+        green = v.get('>', 0)
+        percent = round(100*green/(red+green), 2)
+
+        link_built = link.format(k,k)
+        row_built = row.format(link_built,
+                               red,
+                               green,
+                               percent)
+
+        temp.append(row_built)
+    temp = '\n'.join(temp)
+
+    return template.replace('{{table}}', temp)
+
 def build_main(stats):
     with open('/Users/coreygirard/Documents/GitHub/breeze-ci/data/index_template.html', 'r') as f:
         template = f.read()
@@ -74,19 +132,34 @@ def build_main(stats):
     return template.replace('{{table}}', temp)
 
 raw_path = '/Users/coreygirard/Documents/GitHub/breeze-ci/report/'
-'''
 template_path = '/Users/coreygirard/Documents/GitHub/breeze-ci/templates/'
 reports = get_raw_reports(raw_path)
 
+rename_lookup = make_rename_lookup()
+#pprint(rename_lookup)
+
+
 stats = {k:to_stats(v) for k,v in reports.items()}
-html = {k:to_html(v) for k,v in reports.items()}
+html = {rename_lookup[k[:-5]]:v for k,v in reports.items()}
+
+data = {}
+for k,v in reports.items():
+    k = rename_lookup[k[:-5]]
+    data[k] = {'lines': v,
+               'stats': to_stats(v)}
 
 #pprint(stats)
-#pprint(html)
+pprint(data)
 #print(list(html.values())[0])
 
+#main = build_main(stats)
+#pprint(main)
 
-main = build_main(stats)
+with open(os.path.join(raw_path, 'data.json'), 'w') as f:
+    json.dump(data, f, indent=4)
+
+
+'''
 with open(os.path.join(template_path, 'index.html'), 'w') as f:
     f.write(main)
 
@@ -94,28 +167,3 @@ for k,v in html.items():
     with open(os.path.join(template_path, k), 'w') as f:
         f.write(v)
 '''
-
-
-
-d = {}
-
-with open(os.path.join(raw_path, 'ls_report.txt')) as f:
-    lines = [line.strip('\n') for line in f]
-
-indices = [i for i,e in enumerate(lines) if e == '']
-indices = [-1] + indices + [len(lines)]
-
-lines = [lines[a+1:b] for a, b in zip(indices,indices[1:])]
-for e in lines:
-    a, b = e[0], e[1:]
-
-    while a[-1] in '/:':
-        a = a[:-1]
-    a = a + '/'
-
-
-    for i in b:
-        filepath = a+i
-        d[filepath.replace('/','_')] = filepath
-
-pprint(d)
--- server.py
+++ server.py
@@ -3,20 +3,71 @@ import os
 import random
 import json
 import re
+from pprint import pprint
 
 
 app = Flask(__name__)
 
+@app.route('/<user>/<repo>')
+def repo(user, repo):
+    with open('report/ls_report.txt', 'r') as f:
+        text = f.read()
+    return text
+    return user + '///' + repo
 
+'''
 #@app.route('/', methods=['POST'])
 @app.route('/home')
 def home():
     return render_template('index.html')
 
 #@app.route('/', methods=['GET'])
-@app.route('/file/<filename>', methods=['GET'])
+@app.route('/file/<filename>')
 def source(filename):
-    return render_template(filename)
+    try:
+        return render_template(filename)
+    except:
+        return "404"
+'''
+
+
+
+@app.route('/webhook', methods=['POST'])
+def hook():
+    j = request.get_json()
+    data = {'repository': {'name':                 j['repository']['name'],
+                           'full_name':            j['repository']['full_name'],
+                           'owner': {'login':      j['repository']['owner']['login'],
+                                     'avatar_url': j['repository']['owner']['avatar_url'],
+                                     'html_url':   j['repository']['owner']['html_url']},
+                           'private':              j['repository']['private'],
+                           'html_url':             j['repository']['html_url'],
+                           'pushed_at':            j['repository']['pushed_at']},
+            'sender': {'login':                    j['sender']['login'],
+                       'avatar_url':               j['sender']['avatar_url'],
+                       'html_url':                 j['sender']['html_url']}}
+    pprint(data)
+    return "200"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+{
+  "repository": {
+    "name": "breeze-ci-example",
+    "full_name": "coreygirard/breeze-ci-example",
+    "owner": {
+      "login": "coreygirard",
+      "avatar_url": "https://avatars2.githubusercontent.com/u/4074644?v=4",
+      "html_url": "https://github.com/coreygirard"
+    },
+    "private": false,
+    "html_url": "https://github.com/coreygirard/breeze-ci-example",
+    "pushed_at": "2018-02-14T21:26:45Z"
+  },
+  "sender": {
+    "login": "coreygirard",
+    "avatar_url": "https://avatars2.githubusercontent.com/u/4074644?v=4",
+    "html_url": "https://github.com/coreygirard"
+  }
+}
